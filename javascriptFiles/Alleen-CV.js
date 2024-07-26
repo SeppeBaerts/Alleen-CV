c1750e9ab0c307aa890303143ed26ee14922f7a0
Seppe Baerts <12202625@student.pxl.be>
2024-07-26 03:32:44 +0200

    Fixed the double tap behavior on Ios, Fixed a typo in the over mij sectie. Added the basics of my thesis to the picture array.

--- javascriptFiles/Alleen-CV.js
+++ javascriptFiles/Alleen-CV.js
@@ -69,7 +69,6 @@ const currentBigPicturecontent = {
 }
 //Todo: Create a video option
 //Todo: Expanding images
-//Todo: Create The "why me" cards
 //Todo: Create a way to put links in the text.
 const timeline = {
     leftSemButton: undefined,
@@ -78,6 +77,7 @@ const timeline = {
     currentSemester: 0,
     timelineData: [
         {
+            title: "Eerste Query",
             text: "19 september 2022 begon mijn avontuur. Ik had nog nooit geprogrammeerd. Wel al vaker mee gespeeld, maar het is natuurlijk wel moeilijk om te beginnen als je niet begrijpt wat '.NET' eigenlijk is... " +
                 "Daarom ben ik toch wel met een klein hartje begonnen aan de opleiding. Ik wilde het enorm graag leren, maar ik had het al ene paar keer via Youtube geprobeerd, en dat liep meestal niet goed, of dat was niet uitbreidbaar... " +
                 "Het klinkt misschien gek, maar ik herinner mij nog dat ik mijn eerste querry schreef en dacht 'Dit resultaat is er door mij gekomen... IK heb dit gedaan...' en opeens gingen alle zorgen weg en zag ik mijzelf iets " +
@@ -94,6 +94,7 @@ const timeline = {
             timelineCategories: [3],
         },
         {
+            title: "Hello world C#",
             text: "Dit was mijn eerste contact met .NET en C#. Ik heb hier eigenlijk enorm veel schrik voor gehad omdat " +
                 "dit altijd als het 'buisvak' werd gezien en iedereen rondom mij toch al wat kennis over coderen had. " +
                 "Hoewel dit misschien wel wat stress met zich mee bracht, was mijn doel niet veranderd en was ik gedreven om het beste te zijn.",
@@ -111,6 +112,7 @@ const timeline = {
             timelineCategories: [2],
         },
         {
+            title: "GamerNamen",
             text: "Dit is de eerste keer dat ik iets buiten de oefeningen heb geprobeerd. We hadden net de replace geleerd bij SQL en " +
                 "het leek mij een cool idee om hiermee alle medewerkers hun namen af te printen als gamer tags, dus gamer wordt g4m3r. Dit " +
                 "heeft me geleerd om te spelen met de code en er zo meer uit te leren. ",
@@ -118,7 +120,7 @@ const timeline = {
                 [
                     {
                         imageSource: "assets/FotosTimeline/Gamername.png",
-                        alt: "querry"
+                        alt: "Een screenshot van een sql-query waar ik namen verander in gamernamen."
                     }
                 ],
             alt: "querry",
@@ -127,6 +129,7 @@ const timeline = {
             timelineCategories: [3],
         },
         {
+            title: "3D-portfolio",
             text: "Hier ben ik met CSS in contact gekomen met de 3D elementen. Het was een heel gedoe om het aan de praat " +
                 "te krijgen met bijvoorbeeld perspectieven etc, maar het zag er enorm nice uit. Het enige jammere was dat het " +
                 "3D view van mijn portfolio, mijn portfolio ook weldegelijk kapot maakte. Vandaar heb ik het er uit moeten halen. ",
@@ -143,6 +146,7 @@ const timeline = {
             timelineCategories: [1],
         },
         {
+            title: "Snake",
             text: "Mijn leerkracht van C# heeft mij en een medestudent deze game gegeven als een opdracht. " +
                 "Dit was omdat wij altijd veel te snel klaar waren met de opdrachten en hij hoopte dat dit ons kon bezighouden. " +
                 "Hoewel ik het eigenlijk 1 of 2 dagen later al had, ben ik er zelfstandig een pauze menu en een settings " +
@@ -169,6 +173,7 @@ const timeline = {
             timelineCategories: [2],
         },
         {
+            title: "BlackJack",
             text: "Blackjack was mijn eerste zeer complexe applicatie. Je moet hier weten dat ik 4 maanden aan’t coderen " +
                 "was en ik dit al had gemaakt. Ik had nog niet eens over een class geleerd. Deze blackjack heeft enorm veel" +
                 " mogelijkheden, zoals een sandbox-modus of je kunt de zwarte randen wegdoen, de achtergrond weglaten, split " +
@@ -207,6 +212,7 @@ const timeline = {
             timelineCategories: [2],
         },
         {
+            title: "Solitaire",
             text: "Aangezien ik toch de foto’s van de kaarten al op mijn computer had staan, was mijn idee eigenlijk om nog een " +
                 "spelletje te maken. Omdat ik toch solitaire aan het spelen was, leek dat mij een goeie keuze. " +
                 "Jammer genoeg werkte dit niet en kreeg je dus deze gekke dingen. Ik heb hier wel enorm veel over intersections enz. " +
@@ -224,6 +230,7 @@ const timeline = {
             timelineCategories: [2],
         },
         {
+            title: "Digitale klok",
             text: "Hoewel de digitale klok misschien niet zo heel geweldig leek. Vond ik het enorm leuk gedaan en vond ik dat het echt leek op een alarmklok, daarom dat het erin moest ",/*Digitale klok is misschien niet echt waaw of moeilijk, but it was a fun project*/
             imgSources:
                 [
@@ -238,6 +245,7 @@ const timeline = {
             timelineCategories: [2],
         },
         {
+            title:"Space Shooters",
             text: "Na de les van het Solitaire spelletje, heb ik geleerd hoe intersections werken. Daarom heb ik daarna geprobeerd om een soort space-shooters spel te maken. " +
                 "Het is aardig gelukt alleen ging het enorm traag en ik begreep niet waarom. Nadat ik mijn leerkracht hierover heb gevraagd heeft hij me uitgelegd dat dat " +
                 "kwam omdat ik alle lazers aan’t vergelijken was met alle vallende objecten. " +
@@ -255,6 +263,7 @@ const timeline = {
             timelineCategories: [2],
         },
         {
+            title:"High end Hire",
             text: "High end hire is het project van WPL 2, Hier moesten we een verkoperswebsite maken in een groep." +
                 " We mochten kiezen tussen HTML,CSS en Javascript, of Vue.js, maar dat hadden we nog nooit gebruikt. Mijn eerste keuze ging dus direct uit naar Vue " +
                 "(aangezien ik dan nieuwe dingen kon leren) en hoewel het misschien niet mijn beste werk was, omdat " +
@@ -272,6 +281,7 @@ const timeline = {
             timelineCategories: [1],
         },
         {
+            title:"Encryption try",
             text: "Tijdens het maken van de High end Hire site ben ik in contact gekomen met encryptie. Aangezien " +
                 "ik wilde weten hoe het werkte en hoe zo iets zou werken ben ik het zelf beginnen maken. Eerst wat gedacht en uitgetekend, en hierna gewoon gecodeerd. " +
                 "Het was niet zo eenvoudig als ik dacht dat het ging zijn, maar zeker en vast een leuke uitdaging. " +
@@ -289,11 +299,12 @@ const timeline = {
             timelineCategories: [2],
         },
         {
+            title: "Anifood -Mobile",
             text: "Dit was mijn project voor C# Mobile, Ik heb een App gemaakt waarmee je kunt checken of je huisdier eten heeft gekregen, " +
                 "hoe vaak die eten heeft gekregen en hoe lang je nog met de zak eten zou komen. Je kunt er ook verschillende families in hebben. " +
                 "Wanneer er een actie wordt uitgevoerd, zoals een dier eten geven of toevoegen, een gezin verlaten, inloggen, een account maken... " +
                 "wordt een achterliggende database (gemaakt met Entity Framework) geüpdate via mijn API. Ik heb in dit project ook gefocust op accessibility voor bijv screenreaders etc. " +
-                "Meer informatie en een complete rundown van de app kun je in deze video zien (je moet het wel even kopiëren en plakken): https://youtu.be/n-W7a24X_3Y",
+                "Meer informatie en een complete rundown van de app kun je in deze video zien (je moet het wel even kopiëren en plakken): \n\nhttps://youtu.be/n-W7a24X_3Y",
             imgSources:
                 [
                     {
@@ -327,6 +338,7 @@ const timeline = {
             timelineCategories: [2],
         },
         {
+            title:"Beats perfect - Groepswerk",
             text: "Deze website was een groepswerk voor C# web, we moesten een website maken met MVC en Blazor. " +
                 "Hierin hebben we ook een API geschreven die de stock regelt en kon je inloggen met Google, facebook..." +
                 "De website is geschreven met MVC, Blazor en Identity framework.",
@@ -344,11 +356,46 @@ const timeline = {
             alt: "",
             date: "16/04/2024",
             sem: 4,
-            timelineCategories: [2,1],
+            timelineCategories: [1,2],
+        },
+        {
+            title:"Eindwerk",
+            text: "De vraag vanuit mijn stageplaats was om 'de resultaten van de compliance checker (een software dat ik in mijn stage heb gemaakt) weer te geven in Power BI.' " +
+                "Aangezien ik altijd het kerstje op de taart wil, heb ik gezegd 'Jazeker, In Real-Time!' 'Terwijl ik Blazor vergelijk met React...' Ik heb mij daar een beetje in mispakt. " +
+                "Iets teveel hooi op mijn vork genomen, maar het is mij gelukt. Een 19/20 voor mijn eindwerk vind ik toch een heel mooi cijfer. " +
+                "Voor de volledige graduaatsproef kun je me altijd een mailtje sturen en dan stuur ik ze met plezier door, ik ben er ook enorm trots op :D. "+
+                "De eerste foto is mijn interface in React, de 2e is dezelfde in Blazor, de 3e en 4e foto's zijn de bijbehorende stroomdiagrammen die ik heb gemaakt om " +
+                "uit te leggen hoe mijn applicatie in elkaar zit. Ik zou hier uren over kunnen doorpraten om eerlijk te zijn, dus als je nog vragen hebt leg ik ze met plezier uit." +
+                "\n \n" +
+                "P.S: Het besef dat ik SignalR ook kon gebruiken in React is pas later in het process gekomen, hierdoor heb ik het niet kunnen toevoegen.",
+            imgSources:
+                [
+                    {
+                        imageSource: "assets/FotosTimeline/Eindwerk/Eindwerk_Res.png",
+                        alt: "de interface van de compliance checker in React."
+                    },
+                    {
+                        imageSource: "assets/FotosTimeline/Eindwerk/Eindwerk_Res_blazor.png",
+                        alt: "de interface van de compliance checker in Blazor."
+                    },
+                    {
+                        imageSource: "assets/FotosTimeline/Eindwerk/schema_react.png",
+                        alt: "de Werking van de React integratie"
+                    },
+                    {
+                        imageSource: "assets/FotosTimeline/Eindwerk/schema_blazor.png",
+                        alt: "de werking van de Blazor integratie"
+                    },
+                ],
+            alt: "",
+            date: "03/06/2024",
+            sem: 4,
+            timelineCategories: [1,2],
         },
 
         /*
         {
+            title:" ",
             text: "under construction",
             imgSources:
                 [
@@ -374,6 +421,7 @@ const timeline = {
     },
     nextSemester: function () {
         timeline.currentSemester = Math.min(4, timeline.currentSemester + 1);
+
         timeline.semText.innerText = "semester " + timeline.currentSemester;
         timeline.applyFilter();
     },
@@ -397,39 +445,7 @@ const timelineCategories = {
     2: "C#",
     3: "SQL",
 }
-const whyMe = [
-    {
-      title:"Leergierigheid",
-      text:"Als IT'er moet je altijd mee zijn met de nieuwste technologie, daarom volg ik de nieuwtjes van C#, .Net en CSS van dichtbij op"
-    },
-    {
-        title:"Innovatief",
-        text:"Ik vind dat er voor bijna elk probleem een oplossing is. Zo heb ik bijvoorbeeld mijn eigen Binaire text extractor gemaakt die de tekst haalt uit oude .PPT bestanden " +
-            "of bijvoorbeeld bij mijn graduaatsproef waarbij ik een PowerBI rapport kon aanmaken, uploaden, data ernaar verzenden en het rapport automatisch vernieuwen zonder dat de " +
-            "user er enige actie voor moest doen. (buiten natuurlijk inloggen)."
-    },
-    {
-        title:"Sociaal",
-        text:"iets drinken na het werk? I'm in! ik vind het altijd leuk om sociaal te doen en ben een echte teamspeler."
-    },
-    {
-        title:"Snelle leerling",
-        text:"Ik ben iemand die goed is in het zien van verbanden of het begrijpen van software. Dit zorgt ervoor dat ik " +
-            "snel een nieuwe kan leren. Ik heb mijn graduaatsproef gemaakt met React.js en Blazor en ik heb gewerkt met Power BI, " +
-            "waar ik ook niks van kende. Ik heb deze dingen geleerd in 4 maanden tijd en dit samen met school."
-    },
-    {
-        title:"Ik hou van uitleggen",
-        text:"Software uitleggen? Geen probleem! ik ben iemand die veel trots neemt in wat hij maakt dus ik leg het ook in alle plezier uit. " +
-            "Zo heb ik vaak mijn medestudenten geholpen bij een probleem dat ze hadden of als ze ergens mee vast zaten."
-    },
-    {
-        title:"Ik weet wat ik codeer",
-        text:"Code kopiëren van ChatGPT kan iedereen, maar begrijpen wat ze plakken is meestal te veel gevraagd. Niet bij mij! " +
-            "Omdat ik zo leergierig ben, wil ik altijd zeker weten wat ik aan het coderen ben, dit zorgt er ook voor dat ik weinig " +
-            "aan ChatGPT moet vragen. Als dat dan toch gebeurd wil ik altijd weten wat ik heb geschreven. Dat bespaard mij ook tijd in de toekomst. "
-    },
-]
+
 /*
 *********************
 * Timeline functions
@@ -441,17 +457,19 @@ function createTimelineImages(timelineArray) {
     timelinePictures.innerHTML = "";
     for (let pic of timelineArray) {
         let container = document.createElement("div");
+        let title = document.createElement("h3");
         let imageNode = createImage(pic.imgSources[0]);
         let textNode = document.createElement("p");
 
         timelinePictures.appendChild(container);
+        container.appendChild(title);
         container.appendChild(imageNode);
         container.appendChild(textNode);
         container.classList.add("small-timeline-image-container");
-
         textNode.textContent = pic.date;
+        title.textContent = pic.title;
 
-        imageNode.addEventListener("click", () => setBigPicture(pic))
+        container.addEventListener("click", () => setBigPicture(pic))
     }
     // setBigPicture(timelineArray[0]);
 }
@@ -473,7 +491,8 @@ function setBigPicture(pic) {
 
 function darkModeCheck() {
     if (!localStorage.getItem("darkMode")) {
-        localStorage.setItem("darkMode", "false");
+        localStorage.setItem("darkMode", "true");
+        toggleDarkmode();
     } else if (localStorage.getItem("darkMode") === "true") {
         toggleDarkmode();
     }
